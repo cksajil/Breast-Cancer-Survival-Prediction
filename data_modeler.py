20c4b7cc82533df348e739d6a105ba0079e4c3ec
cksajil <sajilck@gmail.com>
2023-08-06 21:52:43 +0530

    Add data modeling script

--- data_modeler.py
+++ data_modeler.py
@@ -1,1 +1,15 @@
 import pandas as pd
+from os import path
+from utils.general import load_config
+
+
+def main():
+    config = load_config("config.yaml")
+    location = config["data_directory"]
+    mutation_file = config["mutation_data"]
+    data = pd.read_csv(path.join(location, mutation_file))
+    print(data)
+
+
+if __name__ == "__main__":
+    main()
